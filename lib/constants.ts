1e150d395bebf84bef8af2ba071d9058950664ab
Otabek Narz <otabeknarz@gmail.com>
2025-06-24 09:20:58 +0500

    changed base url

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,7 +4,7 @@
  */
 
 // Base URL for the backend API
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "https://api.11-tutors.com";
 
 // Authentication endpoints
 export const AUTH_ENDPOINTS = {
